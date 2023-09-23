1cb16eb4ef77c3a2caae1f8700c26d8c65b618d8
stargazer <a974881831@gmail.com>
2023-09-23 06:38:58 -0500

    Update yyzsck.js

--- yyzsck.js
+++ yyzsck.js
@@ -1,16 +1,6 @@
-/*
-用药助手13.15
-脚本作者：@ios151 
-特别说明：CK且用且珍惜,<临床指南→指南专题>没有100%解锁.如需解锁完整版单独使用yyzsck.js版
-免责声明：仅供学习 请勿传播，请勿在闲鱼售卖
-更新时间: 2023.9.10.23.26
-
 [rewrite_local]
-#纯会员数据共享(单独使用的话放本地)
 ^https://newdrugs.dxy.cn url script-request-header https://raw.githubusercontent.com/Yu9191/Script/main/yyzsck.js
-#修改个人用户信息
 ^https://newdrugs.dxy.cn url script-response-body https://raw.githubusercontent.com/Yu9191/Script/main/yyzs2.js
-#pdf获取 测试远程不行 放本地用吧
 #^https:\/\/file1\.dxycdn\.com\/ url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/pdf-url.js
 
 [mitm]
